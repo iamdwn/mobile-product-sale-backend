ba88e1468698618d65e3d411b37de5ff82421da8
iamdwn <truongdinhdongduong@gmail.com>
2024-10-14 15:13:12 +0700

    Initialize source code

---
+++ ProductSale.Api/ProductSale.Data/Base/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿
+
+using ProductSale.Api.Models;
+
+namespace ProductSale.Data.Base
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Cart> CartRepository { get; }
+        IGenericRepository<Cartitem> CartitemRepository { get; }
+        IGenericRepository<Category> CategoryRepository { get; }
+        IGenericRepository<Chatmessage> ChatmessageRepository { get; }
+        IGenericRepository<Notification> NotificationRepository { get; }
+        IGenericRepository<Order> OrderRepository { get; }
+        IGenericRepository<Payment> PaymentRepository { get; }
+        IGenericRepository<Product> ProductRepository { get; }
+        IGenericRepository<Storelocation> StorelocationRepository { get; }
+        IGenericRepository<User> UserRepository { get; }
+
+        void Save();
+    }
+}
---
+++ ProductSale.Api/ProductSale.Data/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductSale.Api.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Address { get; set; }
+
+    public string Role { get; set; } = null!;
+
+    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
+
+    public virtual ICollection<Chatmessage> Chatmessages { get; set; } = new List<Chatmessage>();
+
+    public virtual ICollection<Notification> Notifications { get; set; } = new List<Notification>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
