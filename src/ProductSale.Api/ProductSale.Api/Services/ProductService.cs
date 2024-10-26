2ac212e35701c4b72095b910727716cadd46fbad
handgun <DungPHASE172122@fpt.edu.vn>
2024-10-27 02:30:39 +0700

    Add ProductService

---
+++ src/ProductSale.Api/ProductSale.Api/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using ProductSale.Api.Services.Interfaces;
+using ProductSale.Data.Base;
+using ProductSale.Data.Models;
+
+namespace ProductSale.Api.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            return Task.FromResult(_unitOfWork.ProductRepository.Get());
+        }
+    }
+}
