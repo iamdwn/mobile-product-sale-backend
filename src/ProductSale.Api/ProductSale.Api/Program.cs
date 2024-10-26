90a1707095b1b01c7538395713d606a4810cf4c2
iamdwn <truongdinhdongduong@gmail.com>
2024-10-27 02:24:09 +0700

    adjust Dockerfile and add cicd

--- src/ProductSale.Api/ProductSale.Api/Program.cs
+++ src/ProductSale.Api/ProductSale.Api/Program.cs
@@ -1,3 +1,4 @@
+
 namespace ProductSale.Api
 {
     public class Program
@@ -7,39 +8,37 @@ namespace ProductSale.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
-            // Add CORS
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll",
-                    builder => builder
-                        .AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
             }
 
-            app.UseCors("AllowAll");
             app.UseSwagger();
             app.UseSwaggerUI();
+
+            // Shows UseCors with CorsPolicyBuilder
+
+            app.UseCors(builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }
+            );
+
             app.UseHttpsRedirection();
-            app.UseRouting();
+
             app.UseAuthorization();
+
             app.MapControllers();
 
             app.Run();
