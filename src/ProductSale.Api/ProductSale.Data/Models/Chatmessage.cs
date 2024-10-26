bafc08708496f23b6169433d993d8a2d6ca726d9
handgun <DungPHASE172122@fpt.edu.vn>
2024-10-27 02:05:22 +0700

    update scaffold db

--- src/ProductSale.Api/ProductSale.Data/Models/Chatmessage.cs
+++ src/ProductSale.Api/ProductSale.Data/Models/Chatmessage.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace ProductSale.Data.Models;
 
-namespace ProductSale.Api.Models;
-
-public partial class Chatmessage
+public partial class ChatMessage
 {
     public int ChatMessageId { get; set; }
 
