d77665ca53d975b09dfc55c61115478d96142e27
iamdwn <truongdinhdongduong@gmail.com>
2024-10-27 00:35:23 +0700

    adjust swagger

--- src/ProductSale.Api/ProductSale.Api/Program.cs
+++ src/ProductSale.Api/ProductSale.Api/Program.cs
@@ -19,9 +19,10 @@ namespace ProductSale.Api
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
