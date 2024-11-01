215bce6129d7e7fd94582cf489212e37dd231575
iamdwn <truongdinhdongduong@gmail.com>
2024-11-02 01:51:00 +0700

    fix: configure connection string

--- src/ProductSale.Api/ProductSale.Api/Program.cs
+++ src/ProductSale.Api/ProductSale.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using ProductSale.Data.Persistences;
 
 namespace ProductSale.Api
 {
@@ -8,11 +10,26 @@ namespace ProductSale.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Database connection string not configured.");
+            }
+
+            builder.Services.AddDbContext<ProductSaleContext>(options =>
+                options.UseSqlServer(connectionString));
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Configuration
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
 
             var app = builder.Build();
 
