eb3fde03bb00366a8fafa65e396851c61aee86ef
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-11-06 22:47:38 +0700

    feat: notification

---
+++ src/ProductSale.Api/ProductSale.Api/Controllers/NotificationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductSale.Api.Services.Interfaces;
+using ProductSale.Data.DTO.RequestModel;
+using ProductSale.Data.DTO.ResponseModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductSale.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+
+    }
+}
