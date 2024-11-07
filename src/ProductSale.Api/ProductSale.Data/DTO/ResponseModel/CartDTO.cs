9b630e598730ba6dc7258f8f440340e7863fb0ab
handgun <DungPHASE172122@fpt.edu.vn>
2024-11-08 05:10:01 +0700

    UPDATE

--- src/ProductSale.Api/ProductSale.Data/DTO/ResponseModel/CartDTO.cs
+++ src/ProductSale.Api/ProductSale.Data/DTO/ResponseModel/CartDTO.cs
@@ -12,6 +12,7 @@ namespace ProductSale.Data.DTO.ResponseModel
 
     public class CartItemDTO
     {
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
